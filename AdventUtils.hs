a55c401a5179ba0cb0d10be2b2038bd7fe5fdf27
caneroj1 <caneroj1@tcnj.edu>
2015-12-05 23:25:13 -0500

    finished part 2 of day 5

--- AdventUtils.hs
+++ AdventUtils.hs
@@ -1,14 +1,20 @@
 module AdventUtils
     (
-      openInputAndExecute      ,
-      conditionForCharPairs    ,
-      Cond(..)
+      openInputAndExecute          ,
+      conditionForCharPairs        ,
+      getCharPairsAndStartPositions,
+      Cond(..)                     ,
+      PairMap
     ) where
 
+import           Data.Map.Strict    as Map
+import           Data.Maybe
 import           System.Environment
 import           System.IO
 
 data Cond = And | Or deriving (Eq)
+type StartPositions = [Int]
+type PairMap = Map.Map String StartPositions
 
 openInputAndExecute :: (String -> IO ()) -> IO ()
 openInputAndExecute fn = do
@@ -27,3 +33,16 @@ conditionForCharPairs fn cond (x:xs)
   where nextChar = head xs
         first = fn (Just x) (Just nextChar)
         second = conditionForCharPairs fn cond xs
+
+getCharPairsAndStartPositions :: PairMap -> Int -> String -> PairMap
+getCharPairsAndStartPositions m _ [] = m
+getCharPairsAndStartPositions m _ [_] = m
+getCharPairsAndStartPositions m start (x:xs) =
+  getCharPairsAndStartPositions (adjustMap m key start) (start + 1) xs
+  where key = [x, head xs]
+
+adjustMap :: PairMap -> String -> Int -> PairMap
+adjustMap m k v
+  | isNothing item = Map.insert k [v] m
+  | otherwise = Map.insert k (fromJust item ++ [v]) m
+  where item = Map.lookup k m
--- December5_Part2.hs
+++ December5_Part2.hs
@@ -1,1 +1,25 @@
 import           AdventUtils
+import qualified Data.Map.Strict as Map
+import           Data.Maybe
+
+main = openInputAndExecute (print . length . filter isNaughtyOrNice . lines)
+
+isNaughtyOrNice :: String -> Bool
+isNaughtyOrNice xs = disjointPairs pairMap && letterCheck xs
+  where pairMap = getCharPairsAndStartPositions Map.empty 0 xs
+
+disjointPairs :: PairMap -> Bool
+disjointPairs m = any noOverlap values
+  where list = Map.toList m
+        values = map snd list
+
+noOverlap :: [Int] -> Bool
+noOverlap [] = False
+noOverlap [_] = False
+noOverlap xs = (last xs - head xs) > 1
+  where disjoint x y = y - x > 1
+
+
+letterCheck :: String -> Bool
+letterCheck [_, _] = False
+letterCheck (x:y:xs) = (x == head xs) || letterCheck (y:xs)
